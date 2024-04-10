9432e26a400bd603bcf83c92ac95b81b42ab6cea
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-04-10 14:03:33 +0100

    🧪 test(res4j-retry): enhances test coverage for retry registry and retry state machine

--- resilience4j/src/test/java/retry/RetryServiceTest.java
+++ resilience4j/src/test/java/retry/RetryServiceTest.java
@@ -4,25 +4,22 @@ import exceptions.NetworkException;
 import exceptions.RemoteServiceException;
 import exceptions.WebServiceException;
 import io.github.resilience4j.core.IntervalFunction;
-import io.github.resilience4j.core.SupplierUtils;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.function.Try;
-import service.RemoteService;
+import org.mockito.invocation.InvocationOnMock;import org.mockito.stubbing.Answer;import service.RemoteService;
 
 import java.time.Duration;
-import java.util.concurrent.Callable;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
 
-import static io.github.resilience4j.core.CallableUtils.recover;
 import static io.github.resilience4j.core.SupplierUtils.recover;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.BDDMockito.then;
+import static io.github.resilience4j.retry.event.RetryEvent.Type;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class RetryServiceTest {
@@ -60,7 +57,7 @@ public class RetryServiceTest {
         try {
 
             // when: the service is called
-            decorated.apply(1);
+            decorated.apply(anyInt());
             fail("Expected an exception to be thrown if all retries failed");
         } catch (Exception e) {
             // then: it should be retried the maximum number of times specified in the retry configuration
@@ -122,7 +119,7 @@ public class RetryServiceTest {
 
         // and: the service is called
         try {
-            decorated.apply(1);
+            decorated.apply(anyInt());
             fail("Expected an exception to be thrown if all retries failed");
         } catch (Exception e) {
             // then: it should be retried the maximum number of times specified in the retry configuration
@@ -164,7 +161,7 @@ public class RetryServiceTest {
 
         // when: the service is called
         try {
-            decorated.apply(1);
+            decorated.apply(anyInt());
             fail("Expected an exception to be thrown if all retries failed");
         } catch (Exception e) {
             // then: it should be retried the maximum number of times specified in the retry configuration
@@ -201,6 +198,7 @@ public class RetryServiceTest {
         String defaultConfigName = "defaultConfig";
         String customConfigName = "customConfig";
         Retry defaultRetry = registry.retry(defaultConfigName, defaultConfig);
+        @SuppressWarnings("unused")
         Retry customRetry = registry.retry(customConfigName, customConfig);
         registry.getAllRetries().forEach(retry -> logger.info("RetryName: " + retry.getName()));
         registry.remove(defaultConfigName);
@@ -220,5 +218,283 @@ public class RetryServiceTest {
                 .onSuccess(event -> logger.info("Success: " + event.getEventType()));
 
         // or retry.getEventPublisher().onEvent(event -> logger.info(event.toString())); to log all events indiscriminately
+    }
+
+    @Test
+    public void retryStateMachineAndContextPreservation() {
+        // given: all types of possible retry events
+        System.out.println("RetryEvent.Type: " + Arrays.toString(Type.values()));
+
+        // and: a retry configuration
+        int maxAttempts = 3;
+        RetryConfig config = RetryConfig.custom()
+                .maxAttempts(maxAttempts)
+                .ignoreExceptions(NetworkException.class)
+                .build();
+
+        // and: a retry instance
+        Retry retry = Retry.of("somename", config);
+
+        // and: a retry list to store the retry event
+        List<Type> retryEvents = new java.util.ArrayList<>();
+
+        // and: a set of listeners are registered to log all retry events
+        retry.getEventPublisher()
+                .onRetry(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Interval: " + event.getWaitInterval()
+                            + " - Event: " + type);
+                    retryEvents.add(type);
+                })
+                .onError(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Error: " + type);
+                    retryEvents.add(type);
+                })
+                .onIgnoredError(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Ignored error: " + type);
+                    retryEvents.add(type);
+                })
+                .onSuccess(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Success: " + type);
+                    retryEvents.add(type);
+                });
+
+        // and: a service is decorated with the retry mechanism
+        RemoteService service = mock(RemoteService.class);
+        Function<Integer, Void> decorated
+                = Retry.decorateFunction(retry, (Integer s) -> {
+            service.process(s);
+            return null;
+        });
+
+        // and: a remote service configuration to always throw an exception to simulate a failure
+        when(service.process(anyInt()))
+                .thenThrow(new WebServiceException("BAM!"));
+
+        // when: the service is called
+        try {
+            decorated.apply(anyInt());
+            fail("Expected an exception to be thrown if all retries failed");
+        } catch (Exception e) {
+            // then: it should be retried the maximum number of times specified in the retry configuration
+            assertEquals(maxAttempts, retryEvents.size());
+            List<Type> expectedList = List.of(
+                    Type.RETRY,
+                    Type.RETRY,
+                    Type.ERROR
+            );
+            assertTrue(expectedList.containsAll(retryEvents));
+        }
+
+        // when: a remote service configuration that does not always throw an exception
+        reset(service);
+        retryEvents.clear();
+        when(service.process(anyInt())).then(new Answer<Integer>() {
+            private int count = 0;
+            public Integer answer(InvocationOnMock invocation) {
+                if (count++ < 2) {
+                    throw new WebServiceException("BAM!");
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        // when: the service is called
+        try {
+            decorated.apply(anyInt());
+        } catch (Exception e) {
+            // then: it should be retried the maximum number of times specified in the retry configuration
+            assertEquals(maxAttempts, retryEvents.size());
+            // and: it should not share the same retry context
+            List<Type> expectedList = List.of(
+                    Type.RETRY,
+                    Type.RETRY,
+                    Type.SUCCESS
+            );
+            assertTrue(expectedList.containsAll(retryEvents));
+        }
+
+        // when: a remote service configuration that throws an ignored exception
+        reset(service);
+        retryEvents.clear();
+        when(service.process(anyInt())).then(new Answer<Integer>() {
+            private int count = 0;
+            public Integer answer(InvocationOnMock invocation) {
+                if (count++ < 2) {
+                    throw new WebServiceException("BAM!");
+                } else {
+                    throw new NetworkException("Thanks Vodafone!");
+                }
+            }
+        });
+
+        // when: the service is called
+        try {
+            decorated.apply(anyInt());
+        } catch (Exception e) {
+            // then: it should be retried the maximum number of times specified in the retry configuration
+            assertEquals(maxAttempts, retryEvents.size());
+            // and: it should not share the same retry context
+            List<Type> expectedList = List.of(
+                    Type.RETRY,
+                    Type.RETRY,
+                    Type.IGNORED_ERROR // which means the retry was successful
+            );
+            assertTrue(expectedList.containsAll(retryEvents));
+        }
+
+    }
+
+    @Test
+    public void ignoredErrorStateIsADifferentTypeOfSucess() {
+        // given: a retry configuration
+        RetryConfig config = RetryConfig.custom()
+                .maxAttempts(3)
+                .ignoreExceptions(NetworkException.class)
+                .build();
+
+        // and: a retry instance
+        Retry retry = Retry.of("somename", config);
+
+        // and: a retry list to store the retry event
+        List<Type> retryEvents = new java.util.ArrayList<>();
+
+        // and: a set of listeners are registered to log all retry events
+        retry.getEventPublisher()
+                .onRetry(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Interval: " + event.getWaitInterval()
+                            + " - Event: " + type);
+                    retryEvents.add(type);
+                })
+                .onError(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Error: " + type);
+                    retryEvents.add(type);
+                })
+                .onIgnoredError(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Ignored error: " + type);
+                    retryEvents.add(type);
+                })
+                .onSuccess(event -> {
+                    Type type = event.getEventType();
+                    logger.info("Success: " + type);
+                    retryEvents.add(type);
+                });
+
+        // and: a service is decorated with the retry mechanism
+        RemoteService service = mock(RemoteService.class);
+        Function<Integer, Void> decorated
+                = Retry.decorateFunction(retry, (Integer s) -> {
+            service.process(s);
+            return null;
+        });
+
+        // and: a remote service configuration that throws an ignored exception
+        when(service.process(anyInt()))
+                .thenThrow(new NetworkException("Thanks Vodafone!"));
+
+        // when: the service is called
+        try {
+            decorated.apply(anyInt());
+            fail("Expected an exception to be thrown if this exception is ignored");
+        } catch (Exception e) {
+            // then: it should not be retried once
+            verify(service, times(1)).process(anyInt());
+
+            // and: it should register the ignored error
+            assertEquals(1, retryEvents.size());
+            assertEquals(Type.IGNORED_ERROR, retryEvents.get(0));
+        }
+    }
+
+    @Test
+    public void retrieveRetryInstancesFromRegistry() {
+        // given: a retry registry
+        RetryRegistry registry = RetryRegistry.ofDefaults();
+
+        // and: a default retry configuration
+        RetryConfig defaultConfig = RetryConfig.ofDefaults();
+
+        // when: the retry is registered with a name
+        String retryName = "myRetry";
+        Retry retry = registry.retry(retryName, defaultConfig);
+
+        // and: the retry is retrieved from the registry using the same name and configuration
+        Retry retryWithSameNameAndConfig = registry.retry(retryName, defaultConfig);
+
+        // then: the retry instances should be strictly equal
+        assertSame(retry, retryWithSameNameAndConfig);
+
+        // when: the retry is retrieved from the registry using the same name and a different configuration
+        RetryConfig newConfig = RetryConfig.custom().maxAttempts(5).build();
+        Retry retryWithSameNameAndDiffConfig = registry.retry(retryName, newConfig);
+
+        // then: the retry instances should be strictly equal
+        assertSame(retry, retryWithSameNameAndDiffConfig);
+        assertEquals(retry.getRetryConfig(), retryWithSameNameAndDiffConfig.getRetryConfig());
+
+        // when: the retry is retrieved from the registry using a different name independent of the configuration
+        String newRetryName = "newRetry";
+        Retry newRetry = registry.retry(newRetryName, defaultConfig);
+
+        // then: the retry instances should not be strictly equal
+        assertNotSame(retry, newRetry);
+    }
+
+    @Test
+    public void retryCreationWithAndWithoutRegistry() {
+        // given: a retry registry
+        RetryRegistry registry = RetryRegistry.ofDefaults();
+
+        // and: a default retry configuration
+        RetryConfig defaultConfig = RetryConfig.ofDefaults();
+
+        // when: the retry is created and registered with a name
+        String retryName = "default";
+        Retry retry = registry.retry(retryName, defaultConfig);
+
+        // and: another retry is created without registration
+        Retry retryWithoutRegistry = Retry.of(retryName, defaultConfig);
+
+        // then: the retry instances should not be strictly equal
+        assertNotSame(retry, retryWithoutRegistry);
+        // and: not equal altogether
+        assertNotEquals(retry, retryWithoutRegistry);
+    }
+
+    @Test
+    public void retryInheritsBaseConfigFromRegistry() {
+        // given: a custom retry configuration
+        RetryConfig baseConfig = RetryConfig.custom()
+                .maxAttempts(3)
+                .failAfterMaxAttempts(true)
+                .build();
+
+        // and: a retry registry with a base configuration
+        RetryRegistry registry = RetryRegistry.of(baseConfig);
+
+        // when: the retry is created using the registry
+        String retryName = "myRetry";
+        Retry retry = registry.retry(retryName);
+
+        // then: the retry should inherit the base configuration
+        assertSame(baseConfig, retry.getRetryConfig());
+
+        // when: the retry is created with a custom configuration from the registry
+        RetryConfig customConfig = RetryConfig.custom()
+                .maxAttempts(5)
+                .failAfterMaxAttempts(false)
+                .build();
+        Retry customRetry = registry.retry("customRetry", customConfig);
+
+        // then: the retry should use the custom configuration instead of the base configuration
+        assertNotEquals(baseConfig.getMaxAttempts(), customRetry.getRetryConfig().getMaxAttempts());
+        assertFalse(customRetry.getRetryConfig().isFailAfterMaxAttempts()); // base configuration had this set to true
     }
 }
