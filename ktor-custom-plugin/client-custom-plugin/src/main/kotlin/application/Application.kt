d2951abab28af4425b547bea9baf4568f92eddec
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-04-13 02:28:27 +0100

    📝 docs(ktor-custom-plugin): adds client and server pipeline architecture and plugin creation documentation

--- ktor-custom-plugin/client-custom-plugin/src/main/kotlin/application/Application.kt
+++ ktor-custom-plugin/client-custom-plugin/src/main/kotlin/application/Application.kt
@@ -3,6 +3,7 @@ package application
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import io.ktor.util.pipeline.*
 import kotlinx.coroutines.runBlocking
 import plugins.CustomHeaderConfigurablePlugin
 import plugins.LoggingHeadersPlugin
@@ -18,7 +19,30 @@ fun main() {
             install(LoggingHeadersPlugin)
             install(ResponseTimePlugin)
         }
-
+        // in order to understand the pipeline,
+        val requestPipeline: HttpRequestPipeline = client.requestPipeline
+        val customPhase = PipelinePhase("CustomPhase")
+        requestPipeline.addPhase(customPhase)
+        requestPipeline.intercept(HttpRequestPipeline.Before) {
+            println("Intercepting before phase in request pipeline")
+        }
+        requestPipeline.intercept(customPhase) {
+            println("Intercepting custom phase in request pipeline")
+        }
+        logPipeline("Request pipeline: ", requestPipeline)
+        logPipeline("Send pipeline: ", client.sendPipeline)
+        logPipeline("Response pipeline: ", client.responsePipeline)
+        logPipeline("Receive pipeline: ", client.receivePipeline)
         client.get("https://ktor.io/")
+    }
+}
+
+private fun logPipeline(name: String, pipeline: Pipeline<*, *>) {
+    val requestPipelinePhases = pipeline.items
+    println(name)
+    requestPipelinePhases.forEach {
+        println("- $it")
+        println("- Interceptors: ${pipeline.interceptorsForPhase(it)}")
+        println()
     }
 }
--- ktor-custom-plugin/server-custom-plugin/src/main/kotlin/application/Application.kt
+++ ktor-custom-plugin/server-custom-plugin/src/main/kotlin/application/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
 import plugins.CustomHeaderPlugin
 import plugins.DataTransformationBenchmarkPlugin
 import plugins.DataTransformationPlugin
@@ -12,7 +13,15 @@ import plugins.SimplePlugin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@Suppress("unused") // Referenced in application.conf/yaml
 fun Application.module() {
+    println("Phases in this pipeline:")
+    this.addPhase(PipelinePhase("CustomPhase"))
+    this.items.forEach(::println)
+    val sendPipeline: ApplicationSendPipeline = this.sendPipeline
+    println("Send pipeline: ${sendPipeline.items}")
+    val receivePipeline: ApplicationReceivePipeline = this.receivePipeline
+    println("Receive pipeline: ${receivePipeline.items}"+ "\n")
     install(SimplePlugin)
     install(RequestLoggingPlugin)
     install(DataTransformationPlugin)
